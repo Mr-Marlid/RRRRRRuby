71070e47c6a2cbd94b3ae2833b4c68d9acc5f15c
Mr-Marlid <78685037+Mr-Marlid@users.noreply.github.com>
2023-03-21 16:43:36 +0300

    Add files via upload

--- lab1_2_2.rb
+++ lab1_2_2.rb
@@ -1,9 +1,14 @@
-def kol_del(a,min)
-	if a > 0 then
-		if min > a%10 then
+def min_digit(a)
+	min = 10
+	return min_digit_2(a,min)
+end
+
+def min_digit_2(a,min)
+	if a > 0 then 
+		if min > a%10 then 
 			min = a%10
 		end
-		kol_del(a/10,min)
+		min_digit_2(a/10,min)
 	else
 		return min
 	end
@@ -11,7 +16,4 @@ end
 
 
 a = gets.chomp.to_i
-kol = 0
-sum = 0
-pr = 1
-puts kol_del(a,10)
+puts min_digit(a)
