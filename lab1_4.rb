f4023fcacbd96aed4b529405c4f1458814d7cb4d
Mr-Marlid <78685037+Mr-Marlid@users.noreply.github.com>
2023-03-28 14:16:19 +0300

    Update lab1_4.rb

--- lab1_4.rb
+++ lab1_4.rb
@@ -13,6 +13,24 @@ def vesh_el(mass)
   
 end
 
+def polozh_del()
+  puts "Введите список чисел через запятую: "
+  numbers = gets.chomp.split(",").map(&:to_i)
+
+  divisors = []
+  
+  numbers.each do |num|
+    (1..num).each do |divisor|
+  # Если делитель положительный и не добавлен в список ранее, добавляем
+      if num % divisor == 0 && divisor > 0 && !divisors.include?(divisor)
+        divisors << divisor
+      end
+    end
+  end
+  return divisors
+end
+
+
 def vvod_massiva()
   puts "Введите размер массива"
   n = gets.chomp.to_i
@@ -24,11 +42,38 @@ def vvod_massiva()
   return mass
 end
 
-mass = vvod_massiva()
-puts "Задание 11"
-puts unique_el(mass)
-puts "Задание 23"
-puts naim_el(mass)
-puts "Задание 35"
-closest_element = vesh_el(mass)
-puts "Ближайший элемент в массиве это #{closest_element}"
+def kvadrat_lista(list)
+  
+  squares = list.select { |num| (0..10).include?(num2) && list.count(num) > 2 }
+  return squares.map { |num| num2 }
+end
+puts "Введите номер задания (11, 23, 35, 47, 59)"
+v = gets.chomp.to_i
+if v == 11 then
+
+  puts "Задание 11"
+  mass = vvod_massiva()
+  puts unique_el(mass)
+elsif v == 23 then
+  puts "Задание 23"
+  mass = vvod_massiva()
+  puts naim_el(mass)
+elsif v == 35 then
+  puts "Задание 35"
+  mass = vvod_massiva()
+  closest_element = vesh_el(mass)
+  puts "Ближайший элемент в массиве это #{closest_element}"
+elsif v == 47 then
+  puts "Задание 47"
+  
+  divisors = polozh_del()
+  puts "Положительные делители чисел:"
+  puts divisors.join(", ")
+elsif v == 59 then
+  puts "Задание 59"
+  list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  res = kvadrat_lista(list)
+  puts res
+else 
+  puts "Нет такого номера"
+end
