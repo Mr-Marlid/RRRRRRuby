8143f52792b2443150be017bd838fcbd5a027397
Mr-Marlid <78685037+Mr-Marlid@users.noreply.github.com>
2023-04-04 16:28:24 +0300

    Create main.rb

---
+++ lab2/main.rb
@@ -0,0 +1,7 @@
+require_relative 'Student.rb'
+
+test1 = Student.new({name:'Дмитрий', surname:'Петренко', lastname:'Алексеевич', phone:'+7 999 999 99 99',
+                     telegram:'@лололошка)'})
+
+test2 = Student.new({name:'Любовь', surname:'Амарян', lastname:'Руслановна',
+                     telegram:'@lublu)'})
