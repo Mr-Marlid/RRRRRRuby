073587eda26ca6daddb0f0bcba71789a880898f6
Mr-Marlid <78685037+Mr-Marlid@users.noreply.github.com>
2023-04-07 16:24:43 +0300

    Update Student.rb

--- lab2/Student.rb
+++ lab2/Student.rb
@@ -3,7 +3,8 @@ class Student
   public_class_method :new
   attr_reader :first_name, :second_name, :last_name
   #открываем сеттеры и геттеры из базового класса
-  public :phone, :telegram, :email, 'id=', 'phone=', 'telegram=', 'email=', 'git=', :set_contacts
+  attr_accessor :phone, :telegram, :email, :git, :id
+  public 'id=', 'phone=', 'telegram=', 'email=', 'git=', :set_contacts
   
   def initialize(last_name: nil, first_name: nil, second_name: nil, id: nil, phone:nil, telegram: nil, email: nil, git:nil)
     raise ArgumentError, "Required fields: first_name, second_name and last_name!" if first_name.nil? || second_name.nil?|| last_name.nil?
