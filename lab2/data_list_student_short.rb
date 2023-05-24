368d16aa6652e109ef2d6b2760063ac79ead1ea7
Mr-Marlid <78685037+Mr-Marlid@users.noreply.github.com>
2023-05-25 00:42:54 +0300

    Update data_list_student_short.rb

--- lab2/data_list_student_short.rb
+++ lab2/data_list_student_short.rb
@@ -1,21 +1,34 @@
-require_relative 'data_list'
-require_relative 'data_table'
+require_relative 'data_list.rb'
+require_relative 'data_table.rb'
 
-class Data_list_student_short < Data_list
-  #9
-  def initialize(objects)
-       super
-  end
-  
-  def get_names
-    @data.first.attributes.keys[1..-1]
+class DataListStudentShort < DataList
+
+  def initialize(source_array)
+    super
+    @names = set_names
   end
 
-  def get_data
-    data = @data.map.with_index(1) do |student, index|
-      [index] + student.attributes.values[1..-1]
+  def arr=(source_array)
+    raise(ArgumentError, 'Массив может содержать только! элементы StudentShort!') unless valid_array?(source_array)
+    super
+    self.data = DataTable.new(@arr)
+    nil
+  end
+
+  protected
+
+  def valid_array?(array)
+    array.all? { |obj| obj.is_a?(StudentShort)}
+  end
+
+  def set_names
+    acc = []
+    count_attr = 1
+    %w[ФИО Git Контакт].each do |attr|
+      acc.push [count_attr, attr]
+      count_attr += 1
     end
-    Data_table.new(data)
+    acc
   end
-end
 
+end
