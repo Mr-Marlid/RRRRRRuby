c7ae596506f858a32543c29895a55f950d963260
Mr-Marlid <78685037+Mr-Marlid@users.noreply.github.com>
2023-05-25 00:10:55 +0300

    Update data_list.rb

--- lab2/data_list.rb
+++ lab2/data_list.rb
@@ -3,8 +3,17 @@ class Data_list
     @data = data.freeze
   end
 
+  def select(number)
+    @data[number]
+  end
+
+  def get_selected
+    @data.map(&:id)
+  end
+
   private
 
   attr_reader :data
   attr_writer :data
 end
+
